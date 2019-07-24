1dcacd4081c9e70285f5713ed80c3bfad050ae22
Platform45-Nic <nicolo@platform45.com>
2019-07-24 14:43:48 +0200

    Added registrations_controller to edit both sign_up and update params. Added corresponding route as well.

--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 20190724094650) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "first_name"
-    t.string "last_name"
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
