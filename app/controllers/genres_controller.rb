1969d4d74f788fc3ae9954c693b8a1cfc30aab2e
Platform45-Nic <nicolo@platform45.com>
2019-07-26 17:28:52 +0200

    Added genre controller. Added controller actions and corresponding views. Added some logic for genre find per user.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,32 @@
+class GenresController < ApplicationController
+
+  def index
+    binding.pry
+    @genre = Genre.all
+  end
+
+  def show
+    @genre = Genre.all
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @user_id = current_user.id
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      flash[:success] = "You have created a Genre"
+      redirect_to index
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def genre_params
+      params.require(:genre).permit(:name, :created_by_id)
+    end
+end
