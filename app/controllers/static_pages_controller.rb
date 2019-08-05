11ed96823ac60e71fa49d04ff53dc13c15962600
Platform45-Nic <nicolo@platform45.com>
2019-08-05 19:12:18 +0200

    Added correct route for root. Added purchased books for a user in the users profile page. Changed the link_to profile to use correct path on all views.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,25 +8,18 @@ class StaticPagesController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(current_user.id)
+    @transaction = Transaction.where(user_id: @user.id)
+  end
+
   def home
     if current_user.present?
       @user = User.find(current_user.id)
       @book = Book.where(author_id: @user)
-      # @user = User.find(current_user.id)
-      # @book = Book.all.find(current_user.id)
     else
       @book = Book.all
-      # @user = get_user_name_from_id(@book.author_id)
-      # @genre = get_genre_name_from_id(@book.genre_id)
-      # @book = Book.all.map { |k, v| {title: v.title, description: v.description, genre: get_genre_name_from_id(v.genre_id), author: get_user_name_from_id(v.author_id)}}
-      # @book += Book.all do |books|
-      #   @user = User.find(books.author_id).first_name
-      #   @book_genre = get_genre_name_from_id(books.genre_id)
-      # end
     end
-    # @book_genre = @book.each do
-    #   get_genre_name_from_id(@book.genre_id)
-    # end
   end
 
   private
